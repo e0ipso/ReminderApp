582e6d33a2d83589a7d24cb18177f32ca0406dd8
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2013-10-11 13:42:23 +0200

    Create the Timer formt.

--- angular/app/js/controllers.js
+++ angular/app/js/controllers.js
@@ -3,8 +3,34 @@
 /* Controllers */
 
 angular.module('reminderApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('TimerFormController', ['$scope', '$log', function($scope, $log) {
+    // Submit function.
+    $scope.saveTimer = function () {
+      var timer = $scope.timer;
+      // Save it using the Chrome extension storage API.
+      var key = timer.id;
+      chrome.storage.sync.set({key: timer}, function() {
+        // Notify that we saved.
+        $log.log(chrome.storage.sync.get(key));
+      });
+    };
+    // Create ID dynamically.
+    $scope.$watch('timer.name', function () {
+      var id = $scope.timer.name;
+      $scope.timer.id = id.replace(/[^a-zA-Z\d]/g, '-').toLowerCase().replace(/(^-+)|(-+$)/, '').replace(/--+/g, '-');
+    });
+    // TODO: Set the title based on the URL
+    $scope.title = 'Add Timer';
+    // Set the default value.
+    // TODO: Populate the loadedTimer based on the URL
+    $scope.timer = {
+      name: '',
+      id: '',
+      description: '',
+      frequency: 0,
+      fromTime: '',
+      toTime: ''
+    };
   }])
   .controller('MyCtrl2', [function() {
 
--- angular/app/js/services.js
+++ angular/app/js/services.js
@@ -5,7 +5,7 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('reminderApp.services', ['ngResource']).
+angular.module('reminderApp.services', []).
   value('version', '0.1').
   factory('timerService', [function(localStorage) {
     return {
